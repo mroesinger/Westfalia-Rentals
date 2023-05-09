792572b3f5713a59b77a4efa6fcc6df8ba80e895
michaelroesinger <mroesinger@gmail.com>
2023-05-09 14:20:39 -0700

    edited render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,6 +8,6 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 # Build commands for back end
-bundle exec install
+bundle install
 bundle exec rake db:migrate 
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
